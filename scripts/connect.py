2746ba22c104d03bb29891d1218b917aba514f7a
Desperationis <smarttdiego@gmail.com>
2022-10-31 14:26:10 -0700

    Add parent dir modules to scripts

--- scripts/connect.py
+++ scripts/connect.py
@@ -1,4 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), ".."))
 from lightdjitellopy import Tello
+
 import time
 
 """
--- scripts/connectWifi.py
+++ scripts/connectWifi.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.join(os.getcwd(), ".."))
 from lightdjitellopy import Tello
 import sys
 
